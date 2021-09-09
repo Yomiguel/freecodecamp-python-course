bc9afb164f48c973e3ea63d093dbd7781ecc0ae7
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-08 19:58:43 -0500

    completed numbers and variables section.

--- variables.py
+++ variables.py
@@ -1,4 +1,6 @@
-#this is a text type variable in python.
+#----------------------------STRING VARIBLES---------------------------#
+
+#this is a string type variable in python.
 name = 'Luis'  
 
 #print the sentence.
@@ -24,3 +26,42 @@ print(len(name))
 
 #this line replaces and prints the specified word with another word.
 print(name.replace('Luis', 'hi, my name is '+ name))
+
+#---------------------------------NUMBER VARIBLES---------------------------------------#
+
+#this is a number type variable in python.
+
+number1 = 78
+
+number2 = 101.22
+
+number3 = 4
+
+number4 = -505
+
+#this line prints four numbers.
+
+print(number1, number2, number3, number4)
+
+#this line prints the sum of two numbers.
+print(number1 + number2)
+
+#this line prints the subtraction of two numbers.
+print(number1 - number2)
+
+#this line prints the product of two numbers.
+print(number1 * number2)
+
+#this line prints the remainder of division of two numbers.
+print(number1 % number2)
+
+#this line prints the absolute value of a number.
+print(abs(number1-number2))
+
+#this line prints the maximum value of a list of numbers.
+print(max(number1, number2, number3, number4))
+
+#this line prints the minimun value of a list of numbers.
+print(min(number1, number2, number3, number4))
+
+
