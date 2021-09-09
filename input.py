2e64dd119047e0332e3ea47a755bdcd3a05bc374
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-08 20:55:47 -0500

    completed section. inputs by users.

---
+++ input.py
@@ -0,0 +1,6 @@
+#this lines initializes two variables requested from the user.
+name = input('please, input your name. ')
+age = input('please, input your age. ')
+
+#this lines prints a sentence compose by two words input by the user.
+print('your name is ' + name + ' and your age is ' + age + ' years old.')
