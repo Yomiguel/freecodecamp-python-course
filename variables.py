fd4e06b8f494f2598cbf60f3e7bd03c1c1658726
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-08 19:15:58 -0500

    completed string variables section

--- variables.py
+++ variables.py
@@ -1,2 +1,26 @@
 #this is a text type variable in python.
 name = 'Luis'  
+
+#print the sentence.
+print('my name is ' + name)
+
+#this is a number type variable in python.
+age = 30 
+
+#print the sentence.
+print('my name is '+ name + ' and i am '+ str(age) + ' years old')
+
+#this line prints the indicated position of the word.
+print(name[2])
+
+#this line prints the word in capital letter.
+print(name.upper())
+
+#this line print the word in lowercase letter.
+print(name.lower())
+
+#this line print the lengt word.
+print(len(name))
+
+#this line replaces and prints the specified word with another word.
+print(name.replace('Luis', 'hi, my name is '+ name))
