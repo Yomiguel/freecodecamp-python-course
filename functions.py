05d71227e77b3ec0289581f7889c4bead60836a4
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-10 21:07:30 -0500

    jumm

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ number_1 = float(input('input the first number: '))
 number_2 = float(input('input the second number: '))
 print(adition(number_1, number_2))
 
-#the sum of two number is define without return.
+#the sum of two numbers is define without return.
 
 def adition():
     number_1 = float(input('input the first number: '))
