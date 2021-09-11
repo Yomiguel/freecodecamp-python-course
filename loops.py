a73dda36624b70a245cd481fe5698689b3cfa6c9
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-10 21:42:49 -0500

    completed loops section

--- loops.py
+++ loops.py
@@ -1,1 +1,31 @@
 #----------------------------------------loops in python----------------------------------------#
+
+#----------------------------------------while----------------------------------------#
+
+#this part of the code prints the numbers from one to ten.
+i = 1
+while i <= 10:
+    print(i)
+    i +=1
+
+#----------------------------------------for----------------------------------------#
+
+#this part of the code prints the numbers from one to ten.
+for i in range (10):
+    print(i+1)
+
+#this part of the code prints the letters in of a word.
+for i in 'luis':
+    print(i)
+
+#this part of the code prints the values in a list.
+names = ['luis', 'miguel']
+for i in names:
+    print(i) 
+
+#this part of the code prints the values in a list and stops at the indicated value.
+names = ['luis', 'miguel', 'jimenez', 'castro']
+for i in names:
+    print(i)
+    if i == 'jimenez':
+        break 
