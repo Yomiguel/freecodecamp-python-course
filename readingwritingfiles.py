e807c0ecbe52bbd7030ab5fe4e0a56129720f727
Luis Miguel Jimenez <yomiguel112@gmail.com>
2021-09-11 10:29:56 -0500

    completed reading files section

--- readingwritingfiles.py
+++ readingwritingfiles.py
@@ -1,2 +1,25 @@
 #-------------------------reading files--------------------------
 
+# 'r' for reading only, 'w' for writing only, 'r+' for writing and reading, 'a' for append.
+file_names = open("names.txt","r+")
+
+#this line print the state of the file (r(readable), w(wirtable), r+ or a).
+print(file_names.writable()) 
+
+#this line print the the first line of the file.
+print(file_names.readline()) 
+
+#this line print the lines of the file as a list.
+print(file_names.readlines()) 
+
+#this part of the code print line by line the file.
+for i in file_names.readlines():
+    print(i) 
+
+#this line print the third line in file.
+print(file_names.readlines()[2])
+
+#this line close the file.
+file_names.close
+
+#-------------------------wrting files--------------------------
